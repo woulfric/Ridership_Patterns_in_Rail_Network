f38d4baaf791fc6cee6996bb6ecafbef60e1476d
kamel yennek <78176618+woulfric@users.noreply.github.com>
2025-12-09 17:23:38 +0100

    first comit

---
+++ main.r
@@ -0,0 +1,8 @@
+
+# Ouverture des fichiers CSV
+
+q1_data <- read.csv("data/validations-reseau-ferre-nombre-validations-par-jour-1er-trimestre.csv", sep = ";")
+q2_data <- read.csv("data/validations-reseau-ferre-nombre-validations-par-jour-2eme-trimestre.csv", sep = ";")
+
+
+View(q2_data)
