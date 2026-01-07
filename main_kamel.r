add643570b904b6a41dd83718a1530b87784bce4
kamel yennek <78176618+woulfric@users.noreply.github.com>
2026-01-07 16:04:13 +0100

    raw data

--- main_kamel.r
+++ main_kamel.r
@@ -1,5 +1,5 @@
 library(tidyverse)
-# library(readr)
+
 # Ouverture des fichiers CSV
 
 # validation_1_trim_2025 = read.csv("data/validations-reseau-ferre-nombre-validations-par-jour-1er-trimestre.csv", sep = ";")
@@ -28,16 +28,16 @@ validation_2_trim_2021 = read.delim("data/data-rf-2021/2021_S2_NB_FER.txt")
 
 #2022
 validation_1_trim_2022 = read.delim("data/data-rf-2022/2022_S1_NB_FER.txt")
-validation_2_trim_2022 = read.delim("data/data-rf-2022/2022_S2_NB_FER.txt")
+#on ajoute un separateur car il est different du reste des fichiers
+validation_2_trim_2022 = read.delim("data/data-rf-2022/2022_S2_NB_FER.txt", sep = ";")
 
 #2023
 validation_1_trim_2023 = read.delim("data/data-rf-2023/2023_S1_NB_FER .txt")
 validation_2_trim_2023 = read.delim("data/data-rf-2023/2023_S2_NB_FER.txt", fileEncoding = "UTF-16LE")
 
-
 #2024
 validation_1_trim_2024 = read.delim("data/data-rf-2024/2024_S1_NB_FER.txt")
-# validation_2_trim_2024 = read.delim("data/data-rf-2024/2024_S2_NB_FER.txt")
+validation_2_trim_2024 = read.delim("data/data-rf-2024/2024_T3_NB_FER.txt")
 
 
 
@@ -49,22 +49,48 @@ liste_dfs <- list(
   validation_1_trim_2021, validation_2_trim_2021,
   validation_1_trim_2022, validation_2_trim_2022,
   validation_1_trim_2023, validation_2_trim_2023,
-  validation_1_trim_2024
-
+  validation_1_trim_2024, validation_2_trim_2024
 )
 
-dim = liste_dfs[[1]] %>% dim()
-
-dim[2]
-
-
-# On parcourt chaque élément de la liste avec i (de 1 jusqu'à la fin de la liste)
 for (i in 1:length(liste_dfs)) {
   
-  # On récupère les dimensions de l'élément i
-  dims <- liste_dfs[[i]] %>% dim()
+  print(paste("--- FICHIER N°", i, "---"))
   
-  # On affiche le résultat proprement
-  # dims[1] = Lignes, dims[2] = Colonnes
-  print(paste("Fichier n°", i, "-> Lignes :", dims[1], "| Colonnes :", dims[2]))
+  # Affiche le type de chaque colonne
+  print(sapply(liste_dfs[[i]], class))
+  
 }
+
+
+clean_valid_type <- function(df) {
+  df %>%
+    # --- CORRECTION ICI ---
+    # On parcourt TOUS les noms de colonnes (.)
+    # Si le nom est dans la liste des intrus, on le change. Sinon on garde l'original.
+    rename_with(~ ifelse(. %in% c("lda", "ID_ZDC"), "ID_REFA_LDA", .)) %>%
+    
+    # --- ETAPE 2 : Types ---
+    mutate(
+      CODE_STIF_RES   = as.character(CODE_STIF_RES),
+      CODE_STIF_ARRET = as.character(CODE_STIF_ARRET),
+      NB_VALD         = as.integer(NB_VALD),
+      JOUR            = as.Date(parse_date_time(JOUR, orders = c("ymd", "dmy")))
+    )
+}
+
+liste_dfs_clean <- map(liste_dfs, clean_valid_type)
+
+for (i in 1:length(liste_dfs_clean)) {
+  
+  print(paste("--- FICHIER N°", i, "---"))
+  
+  # Affiche le type de chaque colonne
+  print(sapply(liste_dfs_clean[[i]], class))
+  
+}
+
+df <- bind_rows(liste_dfs_clean)
+View(head(validation_2_trim_2022))
+
+# Export du fichier avant nettoyage
+# write_csv(df, "data/final_data/validations_raw_2018_2024.csv")
